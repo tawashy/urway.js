7333ec8763429e5e0652ce49f1304bb5fb4c5def
Yaser Tawash <yaser.tawash@gmail.com>
2023-01-17 23:42:35 +0300

    fix: fixed import issue

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export { URWAY } from "./src";
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.URWAY = void 0;
+var src_1 = require("./src");
+Object.defineProperty(exports, "URWAY", { enumerable: true, get: function () { return src_1.URWAY; } });
