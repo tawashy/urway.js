d2ff7b04cf0be0804a856e3f84377a7c7c93bd10
Yaser Tawash <yaser.tawash@gmail.com>
2023-01-17 23:42:35 +0300

    fix: fixed import issue

--- dist/src/api.js
+++ dist/src/api.js
@@ -4,10 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.api = void 0;
-var axios_1 = __importDefault(require("axios"));
-var api = function (url, data) {
-    return axios_1.default
-        .post("".concat(url, "/URWAYPGService/transaction/jsonProcess/JSONrequest"), data)
-        .then(function (response) { return response.data; });
-};
+const axios_1 = __importDefault(require("axios"));
+const api = (url, data) => axios_1.default
+    .post(`${url}/URWAYPGService/transaction/jsonProcess/JSONrequest`, data)
+    .then((response) => response.data);
 exports.api = api;
