74adcbe43fcc4cb7f5a74c52e71019ce091beb8b
Yaser Tawash <yaser.tawash@gmail.com>
2023-01-21 19:41:03 +0300

    chore: removed unused files

--- dist/src/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TESTING_URL = exports.PRODUCTION_URL = void 0;
-exports.PRODUCTION_URL = "https://payments.urway-tech.com";
-exports.TESTING_URL = "https://payments-dev.urway-tech.com";
